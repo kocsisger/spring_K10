c2932f343b0ce8fba8ba80cf80a622d31cb2c15f
Kocsis Gergely <kocsisger@example.com>
2025-11-04 11:00:18 +0100

    Person Repository works

--- src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/SpringJavaFxApplication.java
@@ -1,13 +1,29 @@
 package hu.unideb.inf.SpringJavaFX;
 
+import hu.unideb.inf.SpringJavaFX.model.Person;
+import hu.unideb.inf.SpringJavaFX.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class SpringJavaFxApplication {
+public class SpringJavaFxApplication implements CommandLineRunner {
+	@Autowired
+	PersonRepository personRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJavaFxApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Person p = new Person();
+		p.setName("Béla");
+		p.setDateOfBirth(LocalDate.of(2000,1,1));
+
+		personRepository.save(p);
+	}
 }
